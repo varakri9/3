97a4d05489dc65fdec57098327d7696ad43524ad
varakri9 <69152953+varakri9@users.noreply.github.com>
2020-09-26 20:59:56 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+var checkboxBtn=document.getElementById("checkbox");
+var body=document.body;
+   
+checkboxBtn.addEventListener("click", function(){
+     body.classList.toggle("light-mode");
+});
+
+
+
+
